c417cbfed87aaba4873a39db62d583875b449f5b
Ensar Gök <116498667+ensargx@users.noreply.github.com>
2026-02-10 12:49:53 +0300

    flag: flag formatı düzeltildi ve flag üretme scripti eklendi

--- reverse/pasli_demir/src/main.rs
+++ reverse/pasli_demir/src/main.rs
@@ -5,9 +5,11 @@ fn main() {
     // let mut xor_keys = std::vec::Vec::<u8>::new();
     // xor_keys.reserve(16);
     let xor_keys = vec![0x1a, 0x69, 0xe5, 0x02, 0x91, 0xde, 0x66, 0x53, 0x3e, 0x77, 0xd4, 0x6a, 0x89, 0x93, 0x64, 0x54];
-    let expected = vec![73, 34, 203, 65, 92, 152, 209, 98, 212, 27, 126, 19, 206, 223, 32, 49, 214, 88, 121, 82, 90, 218, 50, 98, 74, 26, 224, 16, 79];
+    let expected = vec![73, 34, 203, 100, 208, 120, 83, 40, 117, 64, 184, 89, 240, 111, 10, 11, 231, 90, 136, 107, 227, 129, 97, 50, 212, 40, 160, 241, 223, 239, 80, 226, 118];
 
     let mut s = String::new();
+
+    println!("Flag'ı giriniz: ");
 
     let _ = std::io::stdout().flush();
     std::io::stdin().read_line(&mut s)
@@ -38,6 +40,6 @@ fn main() {
 
     assert!(expected == result, "en fazla DoS olurum");
 
-    println!("Tebrikler, paslanmamışsınız!");
+    println!("Tebrikler, paslanmamışsınız. Birden fazla flag olabilir ancak sadece biri doğru ;)");
 
 }
