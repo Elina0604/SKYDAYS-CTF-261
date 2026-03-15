9ce37254df35f55c0190dd78cd0cf7b5d69be7d1
faruk <faruk.omer.erdem@proton.me>
2026-03-15 20:40:36 +0300

    wireguard firewall

--- ctf-utils/wireguard-vpn/firewall.sh
+++ ctf-utils/wireguard-vpn/firewall.sh
@@ -7,21 +7,30 @@ SKY_SEC_SUBNET='10.19.11.32/27'
 
 iptables -F
 iptables -X
-
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
 
+# INPUT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT  # ← EKLENDİ
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p udp --dport 51820 -j ACCEPT
 
+# FORWARD - stateful
 iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+# SKY_SEC tam erişim
 iptables -A FORWARD -s $SKY_SEC_SUBNET -j ACCEPT
+
+# USERS → sadece izinli subnetler
+iptables -A FORWARD -s $USERS_SUBNET -d $CHALLANGES_SUBNET -j ACCEPT
+iptables -A FORWARD -s $USERS_SUBNET -d $OTHER_UTILS_SUBNET -j ACCEPT
+iptables -A FORWARD -s $USERS_SUBNET -j DROP
+
+# Diğer subnetler → USERS
 iptables -A FORWARD -s $OTHER_UTILS_SUBNET -d $USERS_SUBNET -j ACCEPT
 iptables -A FORWARD -s $CHALLANGES_SUBNET -d $USERS_SUBNET -j ACCEPT
-iptables -A FORWARD -s $USERS_SUBNET -d $USERS_SUBNET -j DROP
 
-# Wireguard Logs
-iptables -I FORWARD -i wg0 -j LOG --log-prefix 'tunnel wireguard iptables: ' --log-level 7
-iptables -I FORWARD -o wg0 -j LOG --log-prefix 'tunnel wireguard iptables: ' --log-level 7
+# LOG en sonda
+iptables -A FORWARD -i wg0 -j LOG --log-prefix 'tunnel wireguard iptables: ' --log-level 7
+iptables -A FORWARD -o wg0 -j LOG --log-prefix 'tunnel wireguard iptables: ' --log-level 7
